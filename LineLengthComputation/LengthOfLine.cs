bf510fb826a2be223167e52b3f7be299b87ade8e
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-08 16:36:17 +0530

    [shashidhar] Add: length of line updated

---
+++ LineLengthComputation/LengthOfLine.cs
@@ -0,0 +1,23 @@
+﻿namespace LengthOfCartesionLine
+{
+    internal class LengthOfLine
+    {
+        private int v1;
+        private int v2;
+        private int v3;
+        private int v4;
+
+        public LengthOfLine(int v1, int v2, int v3, int v4)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+
+        internal void calculateLength()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LineLengthComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using LineLengthComputation;
+using System;
+namespace LineLengthComputation
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LineComputationn lineComparison = new LineComputationn(2, 5, 6, 8);
+            lineComparison.calculateLength();
+        }
+    }
+}
